dd308aa7230954abdd728ef365d0ed87e93d0989
KyawZinLin3 <kyawzinlin14future@gmail.com>
2024-02-19 22:43:17 +0630

    learning

--- crud/src/App.jsx
+++ crud/src/App.jsx
@@ -5,21 +5,77 @@ import "./App.css";
 
 function App() {
   const [employees, setEmployees] = useState([]);
+  const [formData, setFormData] = useState({ name: "", salary: "" });
 
+  const getEmployees = async () => {
+    try {
+      const resp = await axios.get("http://localhost:3000/employees");
+      console.log(resp.data);
+      setEmployees(resp.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const createEmployees = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post(
+        "http://localhost:3000/employees",
+        formData
+      );
+      getEmployees();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleFormDataChange = (key, value) => {
+    setFormData((data) => {
+      return { ...data, [key]: value };
+    });
+  };
   useEffect(() => {
-    const getEmployees = async () => {
-      try {
-        const data = await axios.get("http://localhost:3000/employees");
-        console.log(data);
-      } catch (error) {
-        console.log(error);
-      }
-    };
     getEmployees();
   }, []);
+
   return (
     <>
-      <h1>Happy</h1>
+      <form onSubmit={createEmployees}>
+        <label>Name : </label>
+        <input
+          type="text"
+          placeholder="Name"
+          value={formData.name}
+          onChange={(e) => handleFormDataChange("name", e.target.value)}
+        ></input>
+        <label>Salary : </label>
+        <input
+          type="text"
+          placeholder="Enter salary"
+          value={formData.salary}
+          onChange={(e) => handleFormDataChange("salary", e.target.value)}
+        ></input>
+        <button type="submit">Submit</button>
+      </form>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Salary</th>
+          </tr>
+        </thead>
+        {employees.map((employee) => {
+          return (
+            <tbody key={employee.id}>
+              <tr>
+                <td>{employee.name}</td>
+                <td>{employee.salary}</td>
+              </tr>
+            </tbody>
+          );
+        })}
+      </table>
     </>
   );
 }
